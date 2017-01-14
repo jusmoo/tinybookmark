2e28ab8c70e67b6ab6df9717fc7431c8170c5183
Aaron19960821 <yuchenwang@Yuchens-MacBook-Pro.local>
2017-01-14 10:45:59 +0800

    the model templete
    the model template of tiny bookmark

---
+++ model/src/main/java/com/ycpy/model/App.java
@@ -0,0 +1,13 @@
+package com.ycpy.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

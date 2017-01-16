b593df21512a4da99bf60cc284590dd749d4d8d9
Aaron19960821 <wyc8094@gmail.com>
2017-01-16 11:51:56 +0800

    operations on objects

--- model/src/main/java/team/ycpy/tinybookmark/model/Folder.java
+++ model/src/main/java/team/ycpy/tinybookmark/model/Folder.java
@@ -1,5 +1,7 @@
 package team.ycpy.tinybookmark.model;
 
+import java.sql.PreparedStatement;
+
 public class Folder {
 	private String Ino;
 	private String Iname;
@@ -36,6 +38,87 @@ public class Folder {
 	
 	public void setFtype(int ft){
 		this.Ftype = ft;
+	}
+	
+	public PreparedStatement insertUser(ConnectServer con){
+		String sql = "insert into User(Ino,Iname,CreaterId,Ftype) values (?,?,?,?);";
+		
+		try {
+			PreparedStatement stmt = con.prepare(sql);
+			stmt.setString(1, this.getIno());
+			stmt.setString(2, getIname());
+			stmt.setString(3, getCreaterId());
+			stmt.setInt(4, getFtype());
+			
+			return stmt;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	
+	public PreparedStatement deleteFolder(ConnectServer con){
+		String sql = "delete from Folder where Ino=?";
+		try{
+			PreparedStatement stmt = con.prepare(sql);
+			stmt.setString(1, this.getIno());
+			return stmt;
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public PreparedStatement updateFolder(ConnectServer con){
+		int index = 1;
+		
+		String sql = "update Folder set ";
+		if(this.getIname()!=null)sql += "Iname=?,";
+		if(this.getFtype()!=-1)sql += "Ftype=?,";
+		if(this.getCreaterId()!=null)sql += "CreaterId=? ";
+		sql += "where Ino=?";
+		PreparedStatement stmt;
+		
+		try{
+			stmt = con.prepare(sql);
+			if(this.getIname()!=null)stmt.setString(index++, this.getIname());
+			if(this.getFtype()!=-1)stmt.setInt(index++, this.getFtype());
+			if(this.getCreaterId()!=null)stmt.setString(index+1, this.getCreaterId());
+			stmt.setString(index, this.getIno());
+			return stmt;
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public PreparedStatement queryFolder(ConnectServer con, String req){
+		int index = 1;
+		
+		String sql = "Select" + req + "from Item where ";
+		if(this.getIno()!=null)sql += "Ino=?,";
+		if(this.getIname()!=null)sql += "Iname=?,";
+		if(this.getFtype()!=-1)sql += "Ftype=?,";
+		if(this.getCreaterId()!=null)sql += "CreaterId=?";
+		
+		PreparedStatement stmt;
+		
+		try{
+			stmt = con.prepare(sql);
+			if(this.getIno()!=null)stmt.setString(index++, this.getIno());
+			if(this.getIname()!=null)stmt.setString(index++, this.getIname());
+			if(this.getFtype()!=-1)stmt.setInt(index++, this.getFtype());
+			if(this.getCreaterId()!=null)stmt.setString(index, this.getCreaterId());
+			return stmt;
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 }
